17c6624081800edaf7ba480af238f03e7bdf2b85
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-11-07 22:43:05 -0300

    fix: jsxSingleQuote is false

--- next.js
+++ next.js
@@ -1,3 +1,5 @@
+const { rules } = require('./default')
+
 module.exports = {
   env: {
     browser: true,
@@ -20,31 +22,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "jsx-a11y", "@typescript-eslint"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        jsxSingleQuote: true,
-        semi: true,
-        trailingComma: "all",
-      },
-    ],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
-      },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-  },
+  rules,
   settings: {
     react: {
       version: "detect",
